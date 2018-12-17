ad9baba04ae026b4ec2e9e2108e1a35ed034a507
bugagashinka <voloshynandrew@gmail.com>
2018-12-17 15:49:10 +0200

    Reset player position after water collide

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+const playerStartPos = { x: 202, y: 380 };
 // Enemies our player must avoid
 var Enemy = function(startPos, speed) {
   // Variables applied to each of our instances go here,
@@ -68,15 +69,21 @@ Player.prototype.handleInput = function(direction) {
 
 Player.prototype.move = function move(newPos) {
   if (newPos.x > 402 || newPos.x < 2 || newPos.y > 380 || newPos.y < 60) {
-    console.log('true');
+    if (newPos.y < 60) this.reset();
     return;
   }
   this.x = newPos.x;
   this.y = newPos.y;
 };
+
+//Reset Player to initial position
+Player.prototype.reset = function() {
+  this.x = playerStartPos.x;
+  this.y = playerStartPos.y;
+};
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-
 var allEnemies = [
   new Enemy({ x: 0, y: 55 }, 80),
   new Enemy({ x: 0, y: 145 }, 100),
@@ -85,7 +92,7 @@ var allEnemies = [
   new Enemy({ x: -250, y: 230 }, 120),
 ];
 // Place the player object in a variable called player
-var player = new Player({ x: 202, y: 380 });
+var player = new Player(playerStartPos);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
